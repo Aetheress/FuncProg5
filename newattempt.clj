ff71400c38a9b9fd0dfe355deaea65b27410e72b
Aetheress <55312233+Aetheress@users.noreply.github.com>
2019-12-31 23:26:35 +0200

    Update newattempt.clj
    The encryption function has been finished and it works correctly. I would like to split the rail function up into separate functions, maybe two - for side rails and middle rails. It would simply make the code look more intelligible. I know I'm pushing it considering the deadline is kind of in 34 minutes but... I think I'll leave that for tomorrow.

--- newattempt.clj
+++ newattempt.clj
@@ -12,26 +12,42 @@
   (- (* rails 2) 2)
 )
 
-(defn rail [collected remainder message rails row]
+(defn rail [collected remainder message rails row shift]
   (if (not= row 1)
     (if (= (count remainder) (count message))
       (rail 
         "" 
         (subs remainder (- row 1)) 
-        message rails row)
+        message rails row "no")
       (if (= rails row)
         (cond
           (>= (count remainder) (+ (getCycle rails) 1))
             (rail 
               (str collected (subs remainder 0 1)) 
               (subs remainder (* (- rails 1) 2))
-              message rails row)
+              message rails row "no")
           (> (count remainder) 0)
             (str collected (subs remainder 0 1))
           :else
             collected
         )
-        nil;;if one of the middle rows
+        (cond
+          (>= (count remainder) (/ (+ (getCycle rails) 1) 2))
+            (cond
+              (= shift "no")
+                (rail
+                  (str collected (subs remainder 0 1))
+                  (subs remainder (- (* (- rails 1) 2) (* (- row 1) 2))) message rails row "yes")
+              (= shift "yes")
+                (rail
+                  (str collected (subs remainder 0 1))
+                  (subs remainder (* (- row 1) 2)) message rails row "no")
+            )
+          (> (count remainder) 0)
+            (str collected (subs remainder 0 1))
+          :else
+            collected
+        )
       )
     )
     (cond
@@ -39,11 +55,31 @@
         (rail
           (str collected (subs remainder 0 1)) 
           (subs remainder (* (- rails 1) 2)) 
-          message rails row)
+          message rails row "no")
       (> (count remainder) 0)
         (str collected (subs remainder 0 1))
       :else
         collected
     )
   )
+)
+(defn combine [result message rails row]
+  (cond
+    (= rails row)
+      (str result 
+        (rail "" message message rails row "no")
+      )
+    (> rails row)
+      (combine 
+        (str result 
+            (rail "" message message rails row "no")
+        ) 
+        message 
+        rails
+        (+ row 1)
+      )
+  )
+)
+(defn encrypt [message rails]
+  (combine "" message rails 1)
 )
