729f454ecd8385bbc56f3acebb96c652fd7f9f25
Aetheress <55312233+Aetheress@users.noreply.github.com>
2020-01-01 16:30:02 +0200

    Update newattempt.clj
    Added comments to make the rail function more intelligible. The function seems massive but really it's kind of the same thing over and over with mild changes. Yes, I'm aware that kind of goes against the entire principle we've been studying throughout the year.

--- newattempt.clj
+++ newattempt.clj
@@ -13,28 +13,28 @@
 )
 
 (defn rail [collected remainder message rails row shift]
-  (if (not= row 1)
-    (if (= (count remainder) (count message))
+  (if (not= row 1);;if the row isnt the first
+    (if (= (count remainder) (count message));;manipulate the string so that the first letter of the rail is in the first position
       (rail 
         "" 
         (subs remainder (- row 1)) 
         message rails row "no")
-      (if (= rails row)
+      (if (= rails row);;if the row is the last
         (cond
-          (>= (count remainder) (+ (getCycle rails) 1))
+          (>= (count remainder) (+ (getCycle rails) 1));;check for whether there are any more letters to get
             (rail 
               (str collected (subs remainder 0 1)) 
               (subs remainder (* (- rails 1) 2))
               message rails row "no")
-          (> (count remainder) 0)
+          (> (count remainder) 0);;acquire the last letter in certain scenarios
             (str collected (subs remainder 0 1))
-          :else
+          :else;;not sure if this has an actual purpose but this is intended for when the remainder is empty
             collected
         )
-        (cond
-          (>= (count remainder) (/ (+ (getCycle rails) 1) 2))
+        (cond;;if the row is one of the middle rows
+          (>= (count remainder) (/ (+ (getCycle rails) 1) 2));;check for whether there are any more letters to get whilst paying attention that middle rows have more letters than side rows
             (cond
-              (= shift "no")
+              (= shift "no");;the shift variable is used to determine which gap size is present at the given moment, as sometimes the middle rows will have cycling gap sizes
                 (rail
                   (str collected (subs remainder 0 1))
                   (subs remainder (- (* (- rails 1) 2) (* (- row 1) 2))) message rails row "yes")
@@ -43,14 +43,14 @@
                   (str collected (subs remainder 0 1))
                   (subs remainder (* (- row 1) 2)) message rails row "no")
             )
-          (> (count remainder) 0)
+          (> (count remainder) 0);;acquire the last letter in certain scenarios
             (str collected (subs remainder 0 1))
           :else
             collected
         )
       )
     )
-    (cond
+    (cond;;if the row is the first row
       (>= (count remainder) (+ (getCycle rails) 1))
         (rail
           (str collected (subs remainder 0 1)) 
@@ -63,6 +63,7 @@
     )
   )
 )
+
 (defn combine [result message rails row]
   (cond
     (= rails row)
